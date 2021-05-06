ff235f63cf845fbae0b3651ec785197f7c5e9d76
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2021-05-06 13:45:09 +0100

    Update test_utilities.py

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -27,10 +27,14 @@ class TestUtilities(unittest.TestCase):
         self.assertEqual(exp_val, result)
 
     @parameterized.expand([
-        (f'{CA.PV_PREFIX}:{CA.PV_DOMAIN}:NAME1', 'NAME1'),
-        ('NAME1', 'NAME1')
+        (f'{CA.PV_PREFIX}{CA.PV_DOMAIN}NAME1', 'NAME1'),
+        (f'{CA.PV_PREFIX}{CA.PV_DOMAIN}A:B:NAME1', 'A:B:NAME1'),
+        (f'{CA.PV_DOMAIN}A:B:NAME1', 'A:B:NAME1'),
+        (f'{CA.PV_PREFIX}A:B:NAME1', 'A:B:NAME1'),
+        ('NAME1', 'NAME1'),
+        ('A:B:NAME1', 'A:B:NAME1')
     ])
-    def test_GIVEN_pv_name_WHEN_get_short_name_THEN_name_without_domain_is_returned(self, input_val, exp_val):
+    def test_GIVEN_pv_name_WHEN_get_short_name_THEN_short_name_is_returned(self, input_val, exp_val):
         # Act
         result = self.utilities.pv_name_without_prefix_and_domain(input_val)
 
@@ -38,21 +42,23 @@ class TestUtilities(unittest.TestCase):
         self.assertEqual(exp_val, result)
 
     @parameterized.expand([
-        ('MOTHER_DEWAR:HE_LEVEL', f'{CA.PV_PREFIX}:{CA.PV_DOMAIN}:MOTHER_DEWAR:HE_LEVEL'),
         (
-            f'{CA.PV_PREFIX}:{CA.PV_DOMAIN}:MOTHER_DEWAR:HE_LEVEL',
-            f'{CA.PV_PREFIX}:{CA.PV_DOMAIN}:MOTHER_DEWAR:HE_LEVEL'
+            'MOTHER_DEWAR:HE_LEVEL',
+            f'{CA.PV_PREFIX}{CA.PV_DOMAIN}MOTHER_DEWAR:HE_LEVEL'),
+        (
+            f'{CA.PV_PREFIX}{CA.PV_DOMAIN}MOTHER_DEWAR:HE_LEVEL',
+            f'{CA.PV_PREFIX}{CA.PV_DOMAIN}MOTHER_DEWAR:HE_LEVEL'
         ),
         (
-            f'{CA.PV_DOMAIN}:MOTHER_DEWAR:HE_LEVEL',
-            f'{CA.PV_PREFIX}:{CA.PV_DOMAIN}:MOTHER_DEWAR:HE_LEVEL'
+            f'{CA.PV_DOMAIN}MOTHER_DEWAR:HE_LEVEL',
+            f'{CA.PV_PREFIX}{CA.PV_DOMAIN}MOTHER_DEWAR:HE_LEVEL'
         ),
         (
-            f'{CA.PV_PREFIX}:MOTHER_DEWAR:HE_LEVEL',
-            f'{CA.PV_PREFIX}:{CA.PV_DOMAIN}:MOTHER_DEWAR:HE_LEVEL'
+            f'{CA.PV_PREFIX}MOTHER_DEWAR:HE_LEVEL',
+            f'{CA.PV_PREFIX}{CA.PV_DOMAIN}MOTHER_DEWAR:HE_LEVEL'
         )
     ])
-    def test_GIVEN_pv_name_WHEN_get_full_name_THEN_return_full_name(self, input_val, exp_val):
+    def test_GIVEN_pv_name_WHEN_get_full_name_THEN_full_name_is_returned(self, input_val, exp_val):
         # Act
         result = self.utilities.get_full_pv_name(input_val)
 
