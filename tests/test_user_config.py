1d05ace0dbd674daf009209f974798dc56dc757e
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2021-06-02 13:30:54 +0100

    Update test_user_config.py
    Patch logger

--- tests/test_user_config.py
+++ tests/test_user_config.py
@@ -8,6 +8,7 @@ from HLM_PV_Import.settings import PVConfig
 class TestUserConfig(unittest.TestCase):
 
     def setUp(self):
+        patch('HLM_PV_Import.user_config.logger').start()
         patcher = patch.object(UserConfig, "__init__", lambda x: None)
         patcher.start()
         self.addCleanup(patcher.stop)
@@ -18,20 +19,18 @@ class TestUserConfig(unittest.TestCase):
         self.config._check_no_duplicate_object_ids()
 
     def test_GIVEN_duplicate_ids_WHEN_check_if_object_ids_unique_THEN_exception_raised(self):
-        with patch('HLM_PV_Import.user_config.logger'):
-            self.config.object_ids = ['a', 'b', 'b']
-            with self.assertRaises(PVConfigurationException):
-                self.config._check_no_duplicate_object_ids()
+        self.config.object_ids = ['a', 'b', 'b']
+        with self.assertRaises(PVConfigurationException):
+            self.config._check_no_duplicate_object_ids()
 
     def test_GIVEN_id_WHEN_check_if_entry_has_object_id_THEN_no_exception(self):
         self.config.object_ids = ['a', 'b', 'c']
         self.config._check_entries_have_object_ids()
 
     def test_GIVEN_no_id_WHEN_check_if_entry_has_object_id_THEN_exception_raised(self):
-        with patch('HLM_PV_Import.user_config.logger'):
-            self.config.object_ids = ['a', 'b', None]
-            with self.assertRaises(PVConfigurationException):
-                self.config._check_entries_have_object_ids()
+        self.config.object_ids = ['a', 'b', None]
+        with self.assertRaises(PVConfigurationException):
+            self.config._check_entries_have_object_ids()
 
     @parameterized.expand([
         ([{PVConfig.OBJ: 1, PVConfig.LOG_PERIOD: 60, PVConfig.MEAS: {'1': 'a'}}],),
@@ -52,11 +51,10 @@ class TestUserConfig(unittest.TestCase):
         ([{PVConfig.OBJ: 3}],)
     ])
     def test_GIVEN_no_pvs_WHEN_check_if_entries_have_measurement_pvs_THEN_exception_raised(self, entries):
-        with patch('HLM_PV_Import.user_config.logger'):
-            self.config.entries = entries
-            self.config.object_ids = [entry[PVConfig.OBJ] for entry in self.config.entries]
-            with self.assertRaises(PVConfigurationException):
-                self.config._check_entries_have_measurement_pvs()
+        self.config.entries = entries
+        self.config.object_ids = [entry[PVConfig.OBJ] for entry in self.config.entries]
+        with self.assertRaises(PVConfigurationException):
+            self.config._check_entries_have_measurement_pvs()
 
     @patch('HLM_PV_Import.user_config.get_object')
     def test_GIVEN_objects_exist_WHEN_check_if_objects_exist_THEN_no_exception(self, mock_obj_res):
@@ -66,11 +64,10 @@ class TestUserConfig(unittest.TestCase):
 
     @patch('HLM_PV_Import.user_config.get_object')
     def test_GIVEN_objects_not_found_WHEN_check_if_objects_exist_THEN_exception_raised(self, mock_obj_res):
-        with patch('HLM_PV_Import.user_config.logger'):
-            self.config.object_ids = ['a', 'b', 'c']
-            mock_obj_res.return_value = None
-            with self.assertRaises(PVConfigurationException):
-                self.config._check_objects_exist()
+        self.config.object_ids = ['a', 'b', 'c']
+        mock_obj_res.return_value = None
+        with self.assertRaises(PVConfigurationException):
+            self.config._check_objects_exist()
 
     @patch('HLM_PV_Import.user_config.get_connected_pvs')
     @patch('HLM_PV_Import.user_config.UserConfig.get_measurement_pvs')
