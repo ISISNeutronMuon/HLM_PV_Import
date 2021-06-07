814848b7e57a01c1bafe0306fcd1bc9e16ef0e28
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2021-06-07 12:27:44 +0100

    Update InstallPython.ps1

--- InstallPython.ps1
+++ InstallPython.ps1
@@ -1,11 +1,10 @@
 # Attribute to https://stackoverflow.com/a/45827384/10568801
 
-[CmdletBinding()] Param(
-    $pythonVersion = "3.8.1"
-    $pythonUrl = "https://www.python.org/ftp/python/$pythonVersion/python-$pythonVersion.exe"
-    $pythonDownloadPath = 'python-$pythonVersion.exe'
-    $pythonInstallDir = "Python$pythonVersion"
-)
+$pythonVersion = "3.8.1"
+$pythonUrl = "https://www.python.org/ftp/python/$pythonVersion/python-$pythonVersion.exe"
+$pythonDownloadPath = 'python-$pythonVersion.exe'
+$pythonInstallDir = "Python$pythonVersion"
+
 
 (New-Object Net.WebClient).DownloadFile($pythonUrl, $pythonDownloadPath)
 & $pythonDownloadPath /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=$pythonInstallDir
